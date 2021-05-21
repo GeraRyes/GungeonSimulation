59a68c12fd6406addf1924f6c5edd972c50accfd
Gera Reyes <gerarys213@gmail.com>
2021-05-21 16:21:37 -0500

    Initial commit

---
+++ Assets/Logic.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logic : MonoBehaviour
+{
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetFloorProbability(int FloorNumber)
+    {
+        switch (FloorNumber)
+        {
+            case 1:
+
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+        }
+    }
+}
