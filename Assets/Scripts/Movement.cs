f260e9c5a015ace65e1585f6d87bc30210214619
CristopherIC <A01611408@itesm.mx>
2021-05-23 17:30:57 -0500

    Character animation

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public Animator animator;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        transform.position = transform.position + horizontal * Time.deltaTime;
+
+    }
+}
